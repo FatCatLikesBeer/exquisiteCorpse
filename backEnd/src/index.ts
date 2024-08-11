6ba4dae2ffc42eb3e0e3d7dc618884825d4ab725
Billy Bagel <itisbilly@gmail.com>
2024-08-11 02:49:50 -0700

    Added cors to backend, frontend: GET & POST talking to API

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,10 +1,13 @@
-import express, { Express, Request, Response } from 'express';
-import path from 'path';
+import express, { Request, Response } from 'express';
 import { ResponseJSON } from './types';
+import cors from 'cors';
 const logger = require('morgan');
 
 const app = express();
 app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const port = 8082;
 
 app.get('/apiv0', (req: Request, res: Response) => {
@@ -22,7 +25,22 @@ app.get('/apiv0/:id', (req: Request, res: Response) => {
     message: `You said '${query}'!`,
   };
   res.json(response);
-})
+});
+
+app.post('/apiv0/', (req: Request, res: Response) => {
+  console.log(req.body);
+  const title = req.body.title;
+  const body = req.body.body;
+  const response = {
+    success: true,
+    message: "I got what you sent",
+    data: {
+      title,
+      body
+    }
+  }
+  res.json(response);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ frontEnd/App.tsx
@@ -0,0 +1,100 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+
+const url: string = 'http://free.local:8082/apiv0/'
+
+function App() {
+  const Send = () => {
+    const [request, setRequest] = useState("Click the button to interact!");
+    const [inputData, setInputData] = useState("");
+    const [getResponse, setGetResponse] = useState("");
+    const [postBody, setPostBody] = useState("");
+    const [postTitle, setPostTitle] = useState("");
+
+    const [resultTitle, setResultTitle] = useState("Nothing here");
+    const [resultBody, setResultBody] = useState("Really empty");
+
+    const handlePOST = () => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: postTitle,
+          body: postBody,
+        }),
+      })
+        .then(response => response.json())
+        .then((data) => {
+          setRequest(data.message);
+          setResultTitle(data.data.title);
+          setResultBody(data.data.body);
+        });
+    }
+
+    const handleGET = () => {
+      const appendedUrl = url + inputData;
+      fetch(appendedUrl)
+        .then(response => response.json())
+        .then(data => setGetResponse(data.message));
+    }
+
+    return (
+      <View>
+        <View style={{ marginTop: 10, marginBottom: 10 }}>
+          <Button onPress={handleGET} title="Click to GET!" />
+          <Text>{getResponse}</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Send something to Echo!"
+            onChangeText={setInputData}
+            value={inputData}
+          />
+        </View>
+        <View style={{ marginTop: 10, marginBottom: 10 }}>
+          <Button onPress={handlePOST} title="Click to POST!" />
+          <Text style={{ paddingBottom: 8, paddingTop: 8 }}>{request}</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Title"
+            onChangeText={setPostTitle}
+            value={postTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Body"
+            onChangeText={setPostBody}
+            value={postBody}
+          />
+          <Text>{resultTitle}</Text>
+          <Text>{resultBody}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 12 }}>App!</Text>
+      <Send />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    padding: 5,
+  },
+});
+
+export default App;
